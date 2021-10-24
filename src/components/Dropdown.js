2e239127411abb6a375f48b2e6cf0dad50e515fd
Vlad <chepurnojv@gmail.com>
2021-10-24 16:58:01 +0300

    Binding an Event Handler

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      console.log(event.target);
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -11,7 +18,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className='item'
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          console.log("item clicked");
+          onSelectedChange(option);
+        }}
       >
         {option.label}
       </div>
