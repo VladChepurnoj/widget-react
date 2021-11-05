048cc5bb52f4d91c21cbf640b67d8c44fb1a5c51
Vlad <chepurnojv@gmail.com>
2021-11-05 14:40:14 +0200

    Handling Command Clicks

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   useEffect(() => {
     const onLocationChange = () => {
-      console.log("Location change");
+      setCurrentPath(window.location.pathname);
     };
 
     window.addEventListener("popstate", onLocationChange);
@@ -12,7 +13,7 @@ const Route = ({ path, children }) => {
       window.removeEventListener("popstate", onLocationChange);
     };
   }, []);
-  return window.location.path === path ? children : null;
+  return currentPath === path ? children : null;
 };
 
 export default Route;
